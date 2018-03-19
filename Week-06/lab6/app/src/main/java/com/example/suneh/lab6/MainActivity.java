d3e3eac72f7b280c7eb377da73b05b3db259f7c8
spatil41 <35303095+spatil41@users.noreply.github.com>
2018-03-18 22:08:41 -0500

    Week06

---
+++ Week-06/lab6/app/src/main/java/com/example/suneh/lab6/MainActivity.java
@@ -0,0 +1,119 @@
+package com.example.suneh.lab6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+import android.os.Handler;
+import android.util.Log;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView ;
+
+    Button start, pause, reset;
+
+    long MillisecondTime, StartTime, TimeBuff, UpdateTime = 0L ;
+
+    Handler handler;
+
+    int Seconds, Minutes, MilliSeconds ;
+
+    ArrayAdapter<String> adapter ;
+
+    String msg = "Android : ";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(msg, "The onCreate() event");
+
+        textView = (TextView) findViewById(R.id.textView1);
+        start = (Button) findViewById(R.id.button3);
+        pause = (Button) findViewById(R.id.button4);
+        reset = (Button) findViewById(R.id.button5);
+        handler = new Handler();
+
+        start.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View view) {
+                StartTime = SystemClock.uptimeMillis();
+                handler.postDelayed(runnable, 0);
+
+                reset.setEnabled(false);
+            }
+        });
+
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                TimeBuff += MillisecondTime;
+
+                handler.removeCallbacks(runnable);
+
+                reset.setEnabled(true);
+            }
+        });
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                MillisecondTime = 0L;
+                StartTime = 0L;
+                TimeBuff = 0L;
+                UpdateTime = 0L;
+                Seconds = 0;
+                Minutes = 0;
+                MilliSeconds = 0;
+
+                textView.setText("00:00:00");
+                //adapter.notifyDataSetChanged();
+            }
+        });
+
+    }
+            public Runnable runnable = new Runnable() {
+
+            public void run() {
+
+                MillisecondTime = SystemClock.uptimeMillis() - StartTime;
+
+                UpdateTime = TimeBuff + MillisecondTime;
+
+                Seconds = (int) (UpdateTime / 1000);
+
+                Minutes = Seconds / 60;
+
+                Seconds = Seconds % 60;
+
+                MilliSeconds = (int) (UpdateTime % 1000);
+
+                textView.setText("" + Minutes + ":"
+                        + String.format("%02d", Seconds) + ":"
+                        + String.format("%03d", MilliSeconds));
+
+                handler.postDelayed(this, 0);
+            }
+
+        };
+
+    public void startService(View view)
+    {
+        startService(new Intent(getBaseContext(), service.class));
+    }
+
+    // Method to stop the service
+    public void stopService(View view)
+    {
+        stopService(new Intent(getBaseContext(), service.class));
+    }
+}
+
---
+++ Week-06/lab6/app/src/main/java/com/example/suneh/lab6/service.java
@@ -0,0 +1,38 @@
+package com.example.suneh.lab6;
+
+/**
+ * Created by suneh on 3/18/2018.
+ */
+import android.app.Service;
+import android.os.IBinder;
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.support.annotation.Nullable;
+import android.widget.Toast;
+
+public class service extends Service
+{
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent)
+    {
+        return null;
+    }
+
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // Let it continue running until it is stopped.
+        Toast.makeText(this, "Service Started", Toast.LENGTH_LONG).show();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "Service Destroyed", Toast.LENGTH_LONG).show();
+    }
+}
+
+
