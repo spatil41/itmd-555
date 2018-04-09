606b770b12a6ea56fd45ea0aab4763b7491d00ef
Suneha Patil <35303095+spatil41@users.noreply.github.com>
2018-04-09 09:17:04 -0500

    Update BookContract.java

--- Week-07/SQLiteLab7/app/src/main/java/com/example/suneh/sqlitelab7/BookContract.java
+++ Week-07/SQLiteLab7/app/src/main/java/com/example/suneh/sqlitelab7/BookContract.java
@@ -13,7 +13,7 @@ public class BookContract {
     private BookContract() {
     }
 
-    // Inner class that defines the table contents
+   
     public static class BookEntry implements BaseColumns {
         public static final String ID = "id";
         public static final String TABLE_NAME = "BooksDatabase";
@@ -21,12 +21,14 @@ public class BookContract {
         public static final String COLUMN_NAME_AUTHOR = "author";
         public static final String COLUMN_NAME_RATING= "rating";
     }
-    // some common queries
+    // create table query
     public static final String SQL_CREATE_TABLE = "CREATE TABLE " + BookEntry.TABLE_NAME +
             "(" + BookEntry.ID + " INTEGER PRIMARY KEY,"+ BookEntry.COLUMN_NAME_RATING + " INTEGER," + BookEntry.COLUMN_NAME_TITLE + " TEXT,"
             + BookEntry.COLUMN_NAME_AUTHOR + " TEXT)";
-
+  // Delete table query
     public static final String SQL_DELETE_TABLE = "DROP TABLE IF EXISTS " + BookEntry.TABLE_NAME;
+    
+    // Delete all the daya from a table 
     public static final String SQL_DELETE_ALLENTERIES = "DELETE FROM " + BookEntry.TABLE_NAME;
 }
 
