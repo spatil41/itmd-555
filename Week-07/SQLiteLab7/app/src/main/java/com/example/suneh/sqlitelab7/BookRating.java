e4957c33d9820f2b111437f77779ccfa867f84bd
Suneha Patil <35303095+spatil41@users.noreply.github.com>
2018-04-09 08:49:54 -0500

    Add files via upload

---
+++ Week-07/SQLiteLab7/app/src/main/java/com/example/suneh/sqlitelab7/BookRating.java
@@ -0,0 +1,93 @@
+package com.example.suneh.sqlitelab7;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.graphics.Color;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.*;
+
+
+/**
+ * Created by suneh on 4/8/2018.
+ */
+
+public class BookRating extends ArrayAdapter<String>
+{
+
+private final Activity Context;
+
+private final java.util.List<Book> books;
+
+public BookRating(Activity context, List book) {
+
+        super(context, R.layout.list, (java.util.List<String>) book ); this.Context = context;
+        this.books = (java.util.List<Book>) book;
+
+
+        }
+public View getView(int position, View view, ViewGroup parent) {
+
+        int iconColor = Color.BLUE;
+        LayoutInflater inflater = Context.getLayoutInflater();
+        View listview = inflater.inflate(R.layout.list, null, true);
+        TextView ListViewid = (TextView) listview.findViewById(R.id.id);
+        TextView ListViewItems = (TextView) listview.findViewById(R.id.title);
+        TextView ListViewInfos = (TextView) listview.findViewById(R.id.author);
+
+        ListViewid.setText(String.valueOf(books.get( position ).id));
+        ListViewItems.setText(books.get( position ).title);
+        ListViewInfos.setText(books.get( position ).author);
+
+
+        if(books.get(position).rating==0){
+
+        }else if(books.get(position).rating==1){
+        ImageView ListViewImage = (ImageView) listview.findViewById(R.id.star1);
+        ListViewImage.setColorFilter(Color.rgb(0, 0, 255));
+        }else if(books.get(position).rating==2){
+        ImageView ListViewImage = (ImageView) listview.findViewById(R.id.star1);
+        ListViewImage.setColorFilter(Color.rgb(0, 0, 255));
+        ImageView ListViewImage2 = (ImageView) listview.findViewById(R.id.star2);
+        ListViewImage2.setColorFilter(Color.rgb(0, 0, 255));
+
+        }else if(books.get(position).rating==3){
+        ImageView ListViewImage = (ImageView) listview.findViewById(R.id.star1);
+        ListViewImage.setColorFilter(Color.rgb(0, 0, 255));
+        ImageView ListViewImage2 = (ImageView) listview.findViewById(R.id.star2);
+        ListViewImage2.setColorFilter(Color.rgb(0, 0, 255));
+        ImageView ListViewImage3 = (ImageView) listview.findViewById(R.id.star3);
+        ListViewImage3.setColorFilter(Color.rgb(0, 0, 255));
+        }else if(books.get(position).rating==4){
+        ImageView ListViewImage = (ImageView) listview.findViewById(R.id.star1);
+        ListViewImage.setColorFilter(Color.rgb(0, 0, 255));
+        ImageView ListViewImage2 = (ImageView) listview.findViewById(R.id.star2);
+        ListViewImage2.setColorFilter(Color.rgb(0, 0, 255));
+        ImageView ListViewImage3 = (ImageView) listview.findViewById(R.id.star3);
+        ListViewImage3.setColorFilter(Color.rgb(0, 0, 255));
+        ImageView ListViewImage4 = (ImageView) listview.findViewById(R.id.star4);
+        ListViewImage4.setColorFilter(Color.rgb(0, 0, 255));
+        }else if(books.get(position).rating==5){
+        ImageView ListViewImage = (ImageView) listview.findViewById(R.id.star1);
+        ListViewImage.setColorFilter(Color.rgb(0, 0, 255));
+        ImageView ListViewImage2 = (ImageView) listview.findViewById(R.id.star2);
+        ListViewImage2.setColorFilter(Color.rgb(0, 0, 255));
+        ImageView ListViewImage3 = (ImageView) listview.findViewById(R.id.star3);
+        ListViewImage3.setColorFilter(Color.rgb(0, 0, 255));
+        ImageView ListViewImage4 = (ImageView) listview.findViewById(R.id.star4);
+        ListViewImage4.setColorFilter(Color.rgb(0, 0, 255));
+        ImageView ListViewImage5 = (ImageView) listview.findViewById(R.id.star5);
+        ListViewImage4.setColorFilter(Color.rgb(0, 0, 255));
+
+        }
+        return listview;
+        };
+
+
+        }
+
